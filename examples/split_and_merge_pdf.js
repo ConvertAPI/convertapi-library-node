374d3332b0d0b00e9d3098d59b12c8f18751af92
Laurynas Butkus <laurynas@baltsoft.com>
2018-08-28 22:32:35 +0300

    Add PDF split and merge example

---
+++ examples/split_and_merge_pdf.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+
+// set your api secret
+var convertapi = require('../lib')(process.env.CONVERT_API_SECRET);
+
+// Example of extracting first and last pages from PDF and then merging them back to new PDF.
+// https://www.convertapi.com/pdf-to-split
+// https://www.convertapi.com/pdf-to-merge
+
+var dir = require('os').tmpdir();
+
+convertapi.convert('split', {
+  File: './examples/files/test.pdf',
+}).then(function (result) {
+  var firstPage = result.files[0];
+  var lastPage = result.files[result.files.length - 1];
+  var files = [firstPage, lastPage];
+
+  return convertapi.convert('merge', { Files: files });
+}).then(function(result) {
+  return result.saveFiles(dir);
+}).then(function(files) {
+  console.log("The PDF saved to " + files);
+});
