562db431c5e96cc89b4731bba805582970bd8913
Laurynas Butkus <laurynas@baltsoft.com>
2020-04-11 14:33:52 +0300

    Add result file stream example

---
+++ examples/get_result_file_stream.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+
+// set your api secret
+var convertapi = require('../lib')(process.env.CONVERT_API_SECRET);
+
+// Example of converting Web Page URL to PDF and reading file data as string
+// https://www.convertapi.com/web-to-pdf
+
+var fromFormat = 'web';
+var conversionTimeout = 180;
+var https = require('https');
+var Stream = require('stream').Transform;
+var params = {
+  Url: 'https://en.wikipedia.org/wiki/Data_conversion',
+  FileName: 'web-example'
+};
+
+convertapi.convert('pdf', params, fromFormat, conversionTimeout)
+  .then(function(result) {
+    result.files.forEach(function(resultFile) {
+      console.log("Result url: " + resultFile.url);
+
+      https.get(resultFile.url, function(response) {
+        var stream = new Stream();
+
+        response.on('data', function(chunk) {
+          stream.push(chunk);
+        });
+
+        response.on('end', function() {
+          var data = stream.read()
+
+          console.log("Downloaded data size: " + data.length);
+        });
+      });
+    });
+  })
+  .catch(function(e) {
+    console.error(e.toString());
+  });
--- examples/index.js
+++ examples/index.js
@@ -4,6 +4,7 @@ const convert_word_to_pdf_and_png = require('./convert_word_to_pdf_and_png');
 const create_pdf_thumbnail = require('./create_pdf_thumbnail');
 const convert_stream = require('./convert_stream');
 const convert_base64_content = require('./convert_base64_content');
+const get_result_file_stream = require('./get_result_file_stream');
 const retrieve_user_information = require('./retrieve_user_information');
 
 module.exports = {
@@ -13,5 +14,6 @@ module.exports = {
   create_pdf_thumbnail,
   convert_stream,
   convert_base64_content,
+  get_result_file_stream,
   retrieve_user_information
 };
